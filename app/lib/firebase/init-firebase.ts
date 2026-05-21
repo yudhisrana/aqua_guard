60d802bfcc24d3430f428c51022ae34231837759
Dipo Yudhisrana (yudhis) <dipoyudhisrana@gmail.com>
2026-05-21 18:13:41 +0700

    refactor Firebase initialization to use environment variables for configuration

--- app/lib/firebase/init-firebase.ts
+++ app/lib/firebase/init-firebase.ts
@@ -1,21 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app"
-import { getAnalytics } from "firebase/analytics"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCFDGHVxbhRQ3n-lOpw0bUGi8kf3I73uhw",
-  authDomain: "aquaguard-1c198.firebaseapp.com",
-  databaseURL:
-    "https://aquaguard-1c198-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "aquaguard-1c198",
-  storageBucket: "aquaguard-1c198.firebasestorage.app",
-  messagingSenderId: "121962297159",
-  appId: "1:121962297159:web:fe08044176d35c07fcbe62",
-  measurementId: "G-DXB82EEEBT",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 }
 
 // Initialize Firebase
