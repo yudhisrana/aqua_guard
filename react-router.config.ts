2e108463ed0c5aff56d60a7fbe776240ae1b5154
Dipo Yudhisrana (yudhis) <dipoyudhisrana@gmail.com>
2026-06-04 16:09:19 +0700

    add future configuration options for improved routing capabilities

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,4 +4,11 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
+  future: {
+    v8_middleware: true,
+    v8_splitRouteModules: true,
+    v8_viteEnvironmentApi: true,
+    v8_passThroughRequests: true,
+    v8_trailingSlashAwareDataRequests: true,
+  },
 } satisfies Config
